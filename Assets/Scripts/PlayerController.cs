2694f76a10286db10a61970cd3b65a22e33db2dd
U-t420-PC\t420 <jc835072@wcupa.edu>
2016-11-10 05:26:10 -0500

    Fixed sync so two instances move players seperately.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 	struct CubeState {
 		public int x;
 		public int y;
@@ -13,12 +14,12 @@ public class PlayerController : MonoBehaviour {
 	float rotationSpeed = 4f;
 	public int score = 0;//kills
 	public GameObject spear;
-	CubeState state;
+	[SyncVar] CubeState state;
 	// Use this for initialization
 	void Start () {
 		InitState ();
 	}
-	void InitState() {
+	[Server] void InitState() {
 		state = new CubeState {
 			x = 0,
 			y = 0,
@@ -28,12 +29,17 @@ public class PlayerController : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-		KeyCode[] arrowKeys = { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow };
-		foreach (KeyCode arrowKey in arrowKeys) {
-			if (!Input.GetKeyDown (arrowKey))
-				continue;
-			state = Move(state, arrowKey);
-		}
+        if (isLocalPlayer)
+        {
+            KeyCode[] arrowKeys = { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.RightArrow, KeyCode.LeftArrow };
+            foreach (KeyCode arrowKey in arrowKeys)
+            {
+                if (!Input.GetKeyDown(arrowKey))
+                    continue;
+                //state = Move(state, arrowKey);
+                CmdMoveOnServer(arrowKey);
+            }
+        }
 		KeyCode[] rotKeys = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
 		foreach (KeyCode rotkey in rotKeys) {
 			if (!Input.GetKeyDown (rotkey))
@@ -45,7 +51,14 @@ public class PlayerController : MonoBehaviour {
 		}
 		SyncState();
 	}
-	void SyncState () {
+
+    [Command]
+    void CmdMoveOnServer(KeyCode arrowKey)
+    {
+        state = Move(state, arrowKey);
+    }
+
+    void SyncState () {
 		if (!attacking) {
 			transform.position = Vector2.Lerp (transform.position, new Vector2 (state.x, state.y), Time.deltaTime * movementSpeed);
 			Quaternion target = Quaternion.Euler (0, 0, state.zRotate);
